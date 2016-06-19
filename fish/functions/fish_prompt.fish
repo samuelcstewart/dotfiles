f9ba6a1fcc42841fceff09e3d67517963ab62ab3
Samuel Stewart <samuelcstewart@icloud.com>
2016-06-19 22:27:41 +1200

    Fish related fixes to vim. bashrc starts fish.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,22 +1,40 @@
-function fish_prompt --description 'Write out the prompt'
-	set -l last_status $status
+# fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
 
-	if not set -q __fish_prompt_normal
-		set -g __fish_prompt_normal (set_color normal)
-	end
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
 
-	# PWD
-	set_color $fish_color_cwd
-	echo -n (prompt_pwd)
-	set_color normal
+function fish_prompt
+  set_color yellow
+  printf '%s' (whoami)
+  set_color normal
+  printf ' at '
 
-	printf '%s ' (__fish_git_prompt)
+  set_color magenta
+  printf '%s' (hostname|cut -d . -f 1)
+  set_color normal
+  printf ' in '
 
-	if not test $last_status -eq 0
-	set_color $fish_color_error
-	end
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
 
-	echo -n '$ '
+  # git
+  set_color normal
+  printf '%s ' (__fish_git_prompt)
 
-	set_color normal
+  # Line 2
+  echo
+  if test $VIRTUAL_ENV
+    printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
+  end
+  printf '↪ '
+  set_color normal
 end
