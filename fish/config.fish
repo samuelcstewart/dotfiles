6acf4b7bd4e46d9e71a51c108f6a535905213ec2
Samuel Stewart <samuelcstewart@icloud.com>
2016-06-21 17:20:47 +1200

    Change fish colours a bit

--- fish/config.fish
+++ fish/config.fish
@@ -17,21 +17,22 @@ if status --is-interactive
     end
   end
 
-  set fish_color_autosuggestion 969896
-  set fish_color_command b294bb
-  set fish_color_comment f0c674
+  set __fish_init_1_50_0 \x1d
+  set fish_color_autosuggestion 9C9C9C
+  set fish_color_command F4F4F4
+  set fish_color_comment B0B0B0
   set fish_color_cwd green
   set fish_color_cwd_root red
-  set fish_color_end b294bb
-  set fish_color_error cc6666
+  set fish_color_end 969696
+  set fish_color_error FFA779
   set fish_color_escape cyan
   set fish_color_history_current cyan
   set fish_color_match cyan
   set fish_color_normal normal
   set fish_color_operator cyan
-  set fish_color_param 81a2be
-  set fish_color_quote b5bd68
-  set fish_color_redirection 8abeb7
+  set fish_color_param A0A0F0
+  set fish_color_quote 666A80
+  set fish_color_redirection FAFAFA
   set fish_color_search_match \x2d\x2dbackground\x3dpurple
   set fish_color_selection \x2d\x2dbackground\x3dpurple
   set fish_color_valid_path \x2d\x2dunderline
@@ -41,11 +42,11 @@ if status --is-interactive
   set fish_pager_color_prefix cyan
   set fish_pager_color_progress cyan
 
-  set -xU LESS_TERMCAP_mb (printf "\e[01;31m")      # begin blinking
-  set -xU LESS_TERMCAP_md (printf "\e[01;31m")      # begin bold
-  set -xU LESS_TERMCAP_me (printf "\e[0m")          # end mode
-  set -xU LESS_TERMCAP_se (printf "\e[0m")          # end standout-mode
-  set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")   # begin standout-mode - info box
-  set -xU LESS_TERMCAP_ue (printf "\e[0m")          # end underline
-  set -xU LESS_TERMCAP_us (printf "\e[01;32m")      # begin underline
+  #set -x LESS_TERMCAP_mb \x1b\x5b01\x3b31m
+  #set -x LESS_TERMCAP_md \x1b\x5b01\x3b31m
+  #set -x LESS_TERMCAP_me \x1b\x5b0m
+  #set -x LESS_TERMCAP_se \x1b\x5b0m
+  #set -x LESS_TERMCAP_so \x1b\x5b01\x3b44\x3b33m
+  #set -x LESS_TERMCAP_ue \x1b\x5b0m
+  #set -x LESS_TERMCAP_us \x1b\x5b01\x3b32m
 end
